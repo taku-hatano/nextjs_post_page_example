0b3d60fa9f667fa08cb8ed30148ba342658041e3
hatano <taku_hatano@forcia.com>
2024-09-09 11:15:44 +0900

    fix: Suspenseも試してみる

--- src/app/route-handler/hono/confirm/route.tsx
+++ src/app/route-handler/hono/confirm/route.tsx
@@ -1,34 +1,64 @@
 /** @jsxImportSource hono/jsx */
 
 import { Hono } from "hono";
+import { Suspense } from "hono/jsx";
 import { jsxRenderer } from "hono/jsx-renderer";
+import { renderToReadableStream } from "hono/jsx/streaming";
 import { handle } from "hono/vercel";
 
 const app = new Hono();
 
-const renderer = jsxRenderer(
-	({ children }) => {
-		return (
-			<html>
-				<head>
-					<title>Hello Hono!</title>
-				</head>
-				<body>{children as any}</body>
-			</html>
-		) as any;
-	},
-	{
-		stream: true,
-	},
-);
-
-app.post("*", renderer);
+// NOTE なぜか機能しない?
+// const renderer = jsxRenderer(
+// 	({ children }) => {
+// 		console.log("Rendering");
+// 		return (
+// 			<html>
+// 				<head>
+// 					<title>Hello Hono!</title>
+// 				</head>
+// 				<body>{children}</body>
+// 			</html>
+// 		);
+// 	},
+// 	{
+// 		stream: true,
+// 	},
+// );
+//
+// app.post("*", renderer);
 
 app.post("*", async (c) => {
 	const body = await c.req.parseBody();
-	console.log(body);
 
-	return c.html(<div>Hello {body.username}!</div>);
+	const stream = renderToReadableStream(
+		<html>
+			<head>
+				<title>Hello Hono!</title>
+			</head>
+			<body>
+				<div>
+					Hello {body.username}!
+					<Suspense fallback={<div>Loading...</div>}>
+						<FetchComponent />
+					</Suspense>
+				</div>
+			</body>
+		</html>,
+	);
+
+	return c.body(stream, {
+		headers: {
+			"Content-Type": "text/html; charset=UTF-8",
+			"Transfer-Encoding": "chunked",
+		},
+	});
 });
+
+async function FetchComponent() {
+	await new Promise((resolve) => setTimeout(resolve, 2000));
+
+	return <div>Fetched!</div>;
+}
 
 export const POST = handle(app);
